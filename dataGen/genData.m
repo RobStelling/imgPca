44f71eb33f2f072566a0bcad98d12afe0cad299e
Roberto Stelling <roberto@stelling.cc>
2018-01-05 11:52:46 -0200

    Final batch data generation scripts (csh+octave)

--- dataGen/genData.m
+++ dataGen/genData.m
@@ -1,18 +1,20 @@
 % Reads nLines of the desired class from CIFAR-10 batch files and returns the corresponding
-% line-first grayscale images, using luminance wheigts (0.3, 0.59, 0.11), line-first data.
-% Assumes filenames are "data_batch_n.mat"
+% line-first grayscale images (using eights), line-first data.
+% Assumes CIFAR-10 filenames are "data_batch_{n}.mat"
 function ndata = genData(nLines, classNumber)
     i = 0; j = 1;
     baseName = 'data_batch_';
     classData = [[]];
-    % Load data from batch files until reaches the desired # of lines
+    % Loads data from batch files until reaches the desired # of lines
     while (i < nLines)
     	load ([baseName num2str(j) '.mat']);
         classData = cat(1, classData, data(labels==classNumber, :));
         i = size(classData, 1);
         j++;
     endwhile
+    % Converts CIFAR data to grayscale using weights for each channel (0.3, 0.59, 0.11)
 	gs_data=classData(1:nLines,1:1024)*0.3+classData(1:nLines,1025:2048)*0.59+classData(1:nLines,2049:3072)*0.11;
+	% Rotates images -90 degrees
 	for k = 1:nLines
 	  ndata(k,:) = reshape(rot90(reshape(gs_data(k,:), 32, 32), -1), 1, []);
 	endfor
--- dataGen/genFiles.sh
+++ dataGen/genFiles.sh
@@ -1,11 +1,26 @@
 #!/bin/csh
-# This batch must be run on CIFAR-10 folder
-# assumes dataConf.m and genData.m are on the same folder
+# This batch must be run on CIFAR-10 folder where all data batch
+# files are located
+# Assumes dataConv.m and genData.m are on the same folder
 # and octave is on the PATH
+
+# Basic parameter check
+if ($#argv != 1) then
+	echo "Usage: $0 <# of lines>"
+	exit -1
+endif
+
+set test = `echo $1 | grep '^[0-9]*$'`
+if (!($test) || ($test == 0)) then
+	echo "<# of lines> ($1) must be a positive integer"
+	exit -2
+endif
+
+# Generate data for all classes (0-9)
 set i = 0
 echo "Generating files..."
 while ($i < 10)
   echo "Class $i"
-  octave dataConv.m 4000 $i > "cif_c${i}_4k.dat"
+  octave dataConv.m $1 $i > "cif_c${i}_${1}.dat"
   @ i = $i + 1
 end
