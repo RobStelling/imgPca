0ba7cfe7c87617a632960dd422be123181894fa7
Roberto Stelling <roberto@stelling.cc>
2018-01-08 15:30:43 -0200

    Added script to generate a single file with data from differente CIFAR-10  categories

---
+++ dataGen/genData.sh
@@ -0,0 +1,22 @@
+#!/bin/csh
+# This batch must be run on CIFAR-10 folder where all data batch
+# files are located.
+# Assumes dataConv.m and genData.m are on the same folder
+# and octave is on the PATH.
+
+# Basic parameter check
+if ($#argv != 1) then
+	echo "Usage: $0 <# of lines>"
+	exit -1
+endif
+
+set test = `echo $1 | grep '^[0-9]*$'`
+if (!($test) || ($test == 0)) then
+	echo "<# of lines> ($1) must be a positive integer"
+	exit -2
+endif
+
+# Generate data for all classes (0-9)
+set i = 0
+echo "Generating file..."
+octave dataConv.m $1 -1 > "cif_${1}.dat"
